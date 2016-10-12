f7f5f93e96aec48ba952b3872b1afe6b90c76317
Nick <nick.benoit14@gmail.com>
2016-10-12 07:32:00 -0600

    first

---
+++ upload.rb
@@ -0,0 +1,7 @@
+require 'dotenv'
+Dotenv.load
+require 'scorm_cloud'
+
+sc = ScormCloud::ScormCloud.new(ENV['APP_ID'], ENV['SECRET'])
+courses = sc.course.get_course_list
+courses.each { |c| puts "#{c.id} #{c.title}"}
